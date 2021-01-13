ddf80cf9feb2a2f66c410f8196a673780d1355e1
renetua <info@realestate.net.ua>
2021-01-13 22:13:45 +0200

    Change #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,4 @@ sitemap: false
 {%- include js/photoswipe-ui-default.min.js -%}
 {%- include js/bootstrap.lightbox.min.js -%}
 {%- include js/bootstrap.smoothscroll.min.js -%}
-{%- include js/realtyua.js -%}
+{%- include js/renetua.js -%}
