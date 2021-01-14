90fa315e1f356f8ed74a8487c3c35a27f30dbf9e
renetua <info@realestate.net.ua>
2021-01-15 00:50:31 +0200

    Change #17

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -1,12 +1,22 @@
+---
+---
 
 $(document).ready(function(){
 $("body").tooltip({selector:'[data-toggle="tooltip"]'});
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
-let isVnUa = document.getElementById('vn-ua').innerHTML === "";
-if (isVnUa) {
-  document.getElementById('vn-ua').innerHTML = "<p>Вебсайт www.realestate.vn.ua ще тільки будується...</p>";
-  document.getElementsByClassName('vn-ua-btn')[0].className = "d-none";
-}
+
+{%- assign srs = site.data.realestate | sort: 'navtitle' -%}
+{%- for sr in srs -%}
+  {%- assign region = sr.url | split: "." | slice: 2, 4 | join: "." | replace: ".", "-" -%}
+  {%- if sr.url != site.url -%}
+  let isVnUa = document.getElementById("{{ region }}").innerHTML === "";
+  if (isVnUa) {
+    document.getElementById("{{ region }}").innerHTML = "<p>Вебсайт {{ site.url }} тільки будується...</p>";
+    document.getElementsByClassName("{{ region }}-btn")[0].className = "d-none";
+  }
+  {%- endif -%}
+{%- endfor -%}
+
 });
