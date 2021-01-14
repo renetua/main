84b0326517de4d65f30654c816538b06d8ef38e9
renetua <info@realestate.net.ua>
2021-01-15 00:35:42 +0200

    Chnage #15

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -4,9 +4,9 @@ $("body").tooltip({selector:'[data-toggle="tooltip"]'});
 $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
-var isVnUa = document.getElementById('vn-ua').innerHTML === "";
+let isVnUa = document.getElementById('vn-ua').innerHTML === "";
 if (isVnUa) {
   document.getElementById('vn-ua').innerHTML = "<p>Вебсайт www.realestate.vn.ua ще тільки будується...</p>";
-  parentElement.getElementsByClassName('vn-ua-btn').classList.add("d-none");
+  document.getElementsByClassName('vn-ua-btn').style.display = 'none';
 }
 });
