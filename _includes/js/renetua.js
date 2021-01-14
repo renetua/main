fc777a98e6874886ec726a13e2132e4c089e0d46
renetua <info@realestate.net.ua>
2021-01-15 01:05:09 +0200

    Change #21

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -5,15 +5,15 @@ $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').colla
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
 
-{%- assign srs = site.data.realestate | sort: 'navtitle' -%}
+{%- assign srs = site.data.realestate -%}
 {%- for sr in srs -%}
   {%- assign region = sr.url | split: "." | slice: 2, 4 | join: "." | replace: ".", "-" -%}
   {%- if sr.url != site.url -%}
-  let is{{ region | remove: "-" }} = document.getElementById("{{ region }}").innerHTML === "";
-  if (is{{ region | remove: "-" }}) {
-    document.getElementById("{{ region }}").innerHTML = "<p>Вебсайт {{ site.url }} тільки будується...</p>";
-    document.getElementsByClassName("{{ region }}-btn")[0].className = "d-none";
-  }
+    var is{{ region | remove: "-" }} = document.getElementById("{{ region }}").innerHTML === "";
+    if (is{{ region | remove: "-" }}) {
+      document.getElementById("{{ region }}").innerHTML = "<p>Вебсайт {{ sr.url }} тільки будується...</p>";
+      document.getElementsByClassName("{{ region }}-btn")[0].className = "d-none";
+    }
   {%- endif -%}
 {%- endfor -%}
 
