120a0918575e14ee5da2c23bb236772b9a8695aa
renetua <info@realestate.net.ua>
2021-01-15 21:39:44 +0200

    Change #84

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
-$("body").tooltip({selector:'[data-toggle="tooltip"]'});
-$('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
-$(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
-$(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
+  $("body").tooltip({selector:'[data-toggle="tooltip"]'});
+  $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
+  $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
+  $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
+});
