2ba6a49827a1a8ebf64ca09c7b652ab51184fa72
renetua <info@realestate.net.ua>
2021-01-15 14:59:30 +0200

    Change #43

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -5,16 +5,16 @@ $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').colla
 $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
 $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
 
-var isifua = document.getElementById("if-ua").children;
-console.log(isifua);
+const isifua = document.getElementById("if-ua").children;
+console.log(isifua.length);
 
 // if (isifua) {
 //   document.getElementById("if-ua").innerHTML = "<p>Вебсайт if-ua тільки будується...</p>";
 //   document.getElementsByClassName("if-ua-btn")[0].className = "d-none";
 // }
 
-var isvnua = document.getElementById("vn-ua").children;
-console.log(isvnua);
+const isvnua = document.getElementById("vn-ua").children;
+console.log(isvnua.length);
 // if (isvnua) {
 //   document.getElementById("vn-ua").innerHTML = "<p>Вебсайт vn-ua тільки будується...</p>";
 //   document.getElementsByClassName("vn-ua-btn")[0].className = "d-none";
