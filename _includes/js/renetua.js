57992032457143238a2d8f92a3d28d7da3aa3f80
renetua <info@realestate.net.ua>
2021-01-23 21:03:24 +0200

    Change #90

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
   $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
   $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
 
+
+  const RSS = require('vanilla-rss');
+  const rssifua = new RSS(
+    document.querySelector("#if-ua-news"),
+    "https://www.realestate.if.ua/feed.xml",
+    {ssl: true, host: 'realestate.if.ua', layoutTemplate: '{entries}', entryTemplate: '<div class="card mb-2"><div class="card-body"><p class="card-text mb-1"><span class="small text-muted">{date}</span></p><h5 class="card-title h6 mb-1"><a href="{url}">{title}</a></h5><p class="card-text mb-0">{shortBodyPlain}</div></div></div>'}
+  );
+  rssifua.render();
+
+});
+
+{%- comment -%}
   {%- assign srs = site.data.realestate -%}
   const RSS = require('vanilla-rss');
   {%- for sr in srs -%}
@@ -14,12 +26,11 @@ $(document).ready(function(){
       const rss{{ loc }} = new RSS(
         document.querySelector("#{{ region }}-news"),
         "{{ sr.url }}/feed.xml",
-        {ssl: true, host: '{{ site.domain }}', layoutTemplate: '{entries}', entryTemplate: '<div class="card mb-2"><div class="card-body"><p class="card-text mb-1"><span class="small text-muted">{date}</span></p><h5 class="card-title h6 mb-1"><a href="{url}">{title}</a></h5><p class="card-text mb-0">{shortBodyPlain}</div></div></div>'}
+        {ssl: true, host: '{{ sr.url | remove: "https://www." }}', layoutTemplate: '{entries}', entryTemplate: '<div class="card mb-2"><div class="card-body"><p class="card-text mb-1"><span class="small text-muted">{date}</span></p><h5 class="card-title h6 mb-1"><a href="{url}">{title}</a></h5><p class="card-text mb-0">{shortBodyPlain}</div></div></div>'}
       );
       rss{{ loc }}.render();
     {%- else -%}
       document.getElementById("{{ region }}-news").innerHTML = '<div class="alert alert-success mb-0" role="alert">Новин про {{ sr.title | replace_first: 'Н', 'н' }} ще немає...</div>';
     {%- endif -%}
   {%- endfor -%}
-
-});
+{%- endcomment -%}
