d64922020f8156e5502da5254afb329f07b311c9
renetua <info@realestate.net.ua>
2021-01-23 20:36:49 +0200

    Change #88

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -4,4 +4,22 @@ $(document).ready(function(){
   $('.nav-tabs>li>a.nav-link').on('click', function(){ $('.navbar-collapse').collapse('hide'); })
   $(document).on('click', function (e) { if ($(e.target).closest(".card").length === 0) { $('.collapse').collapse('hide'); } });
   $(document).ready(function(){ $('.toast').toast('show'); $('.alert').alert(); });
+
+  {%- assign srs = site.data.realestate -%}
+  const RSS = require('vanilla-rss');
+  {%- for sr in srs -%}
+    {%- assign region = sr.url | split: "." | slice: 2, 4 | join: "." | replace: ".", "-" -%}
+    {%- assign loc = region | remove: "-" -%}
+    {%- if sr.slug and sr.slug != '' -%}
+      const rss{{ loc }} = new RSS(
+        document.querySelector("#{{ region }}-news"),
+        "{{ sr.title }}/feed.xml",
+        {}
+      );
+      rss{{ loc }}.render();
+    {%- else -%}
+      document.getElementById("{{ region }}-news").innerHTML = '<div class="alert alert-success mb-0" role="alert">Новин про {{ sr.title | replace_first: 'Н', 'н' }} ще немає...</div>';
+    {%- endif -%}
+  {%- endfor -%}
+
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,7 @@ sitemap: false
 {%- include js/jquery.min.js -%}
 {%- include js/popper.min.js -%}
 {%- include js/bootstrap.min.js -%}
-{%- comment -%}
-{%- include js/photoswipe.min.js -%}
-{%- include js/photoswipe-ui-default.min.js -%}
-{%- include js/bootstrap.lightbox.min.js -%}
-{%- endcomment -%}
 {%- include js/bootstrap.smoothscroll.min.js -%}
 {%- include js/ua.home.js -%}
+{%- include js/rss.global.min.js -%}
 {%- include js/renetua.js -%}
