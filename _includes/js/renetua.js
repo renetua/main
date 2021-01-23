8b9e45156ce29534aa8feddaaee08c5364663694
renetua <info@realestate.net.ua>
2021-01-23 22:06:13 +0200

    Change #100

--- _includes/js/renetua.js
+++ _includes/js/renetua.js
@@ -10,7 +10,7 @@ window.onload = function() {
   const rssifua = new RSS(
     document.querySelector("#if-ua-news"),
     "https://www.realestate.if.ua/feed.xml",
-    {dateFormatFunction:function(date){},layoutTemplate:'{entries}',entryTemplate:'<div class="card border-0"<div class="card-body px-0 pt-0 pb-1"><p class="card-text mb-1"><span class="small text-muted">{date} / {}</span></p><h5 class="card-title h6 mb-1"><a href="{url}">{title}</a></h5><p class="card-text mb-0">{shortBodyPlain}</p></div></div>'}
+    {dateFormatFunction:function(date){},layoutTemplate:'{entries}',entryTemplate:'<div class="card border-0"<div class="card-body px-0 pt-0 pb-1"><p class="card-text mb-1"><span class="small text-muted">{date}</span></p><h5 class="card-title h6 mb-1"><a href="{url}">{title}</a></h5><p class="card-text mb-0">{shortBodyPlain}</p></div></div>'}
   );
   rssifua
     .on('date', (date) => {
